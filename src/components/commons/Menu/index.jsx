d39d43cf6a59fe9630e29d82ee3f08098fc9cdd9
sloli-sonnguyen <sloli.sonnguyen@gmail.com>
2020-07-25 10:47:34 +0700

    complete logout method

--- src/components/commons/Menu/index.jsx
+++ src/components/commons/Menu/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useHistory } from 'react-router-dom';
 // import bellSVG from '../../../assets/images/menu/bell.svg';
 // import bookmarkSVG from '../../../assets/images/menu/bookmark.svg';
 // import eventSVG from '../../../assets/images/menu/event.svg';
 // import groupSVG from '../../../assets/images/menu/group.svg';
 import homeSVG from '../../../assets/images/menu/home.svg';
 import userSVG from '../../../assets/images/menu/user.svg';
+import logoutSVG from '../../../assets/images/logout.svg';
 import './style.scss';
 //import PropTypes from 'prop-types';
 
@@ -37,12 +38,30 @@ function renderMenu(list, location) {
     });
 }
 
+
+
 function Menu(props) {
     let location = useLocation();
+    let history = useHistory();
+
+    function onLogout() {
+        localStorage.removeItem('accessToken');
+        localStorage.removeItem('userInfo');
+
+        history.push('/login');
+    }
     return (
         <div className="menu">
             <ul className="wrap-content">
                 {renderMenu(list, location)}
+                <li className="menu__item" onClick={onLogout}>
+                    <a href="#" className="link">
+                        <div className="icon" style={{ backgroundColor: '#9a7f92' }}>
+                            <img src={logoutSVG} style={{ marginLeft: '3px' }} alt="icon" />
+                        </div>
+                        <p>Đăng xuất</p>
+                    </a>
+                </li>
             </ul>
         </div>
     );
