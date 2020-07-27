fe5958961fd801f4372b43e15d268adaa3ca7229
sloli-sonnguyen <sloli.sonnguyen@gmail.com>
2020-07-27 21:01:51 +0700

    complete edit name user

--- src/components/Profile/BoxInfo/index.jsx
+++ src/components/Profile/BoxInfo/index.jsx
@@ -1,68 +1,119 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import cameraSVG from '../../../assets/images/camera.svg';
-import editSVG from '../../../assets/images/edit.svg';
-import { openUploadWidget } from '../../../utils/CloudinaryService';
-import axios from 'axios';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import cameraSVG from "../../../assets/images/camera.svg";
+import editSVG from "../../../assets/images/edit.svg";
+import { openUploadWidget } from "../../../utils/CloudinaryService";
+import axios from "axios";
 
 BoxInfo.propTypes = {
-    onUploadImage: PropTypes.func,
-    userInfo: PropTypes.object
+  onUploadImage: PropTypes.func,
+  userInfo: PropTypes.object,
 };
 
-
-
 function BoxInfo(props) {
+  const { userInfo, onUploadImage, isMe } = props;
+  const [inputContent, setInputContent] = useState("");
+  const [statusEditBox, setStatusEditBox] = useState(false);
 
-    const { userInfo, onUploadImage, isMe } = props;
+  // upload image
+  function beginUpload(type) {
+    const uploadOptions = {
+      cloudName: process.env.REACT_APP_CLOUD_NAME,
+      uploadPreset: "pnto4uty",
+    };
 
-    // upload image
-    function beginUpload(type) {
-        const uploadOptions = {
-            cloudName: process.env.REACT_APP_CLOUD_NAME,
-            uploadPreset: 'pnto4uty'
-        };
+    openUploadWidget(uploadOptions, (error, photos) => {
+      if (!error) {
+        if (photos.event === "success") {
+          const imageUrl = photos.info.url;
+          const url =
+            process.env.REACT_APP_API_URL + `users/${userInfo.id}/update`;
+          // gui du lieu len server
+          axios.post(url, { [type]: imageUrl }).then((res) => {
+            console.log(res, "oke");
+            const newUserInfo = {
+              ...userInfo,
+              [type]: imageUrl,
+            };
+            // chuyen userinfo cho component cha quan ly
+            onUploadImage(newUserInfo);
+          });
+        }
+      } else {
+        console.log(error);
+      }
+    });
+  }
 
-        openUploadWidget(uploadOptions, (error, photos) => {
-            if (!error) {
-                if (photos.event === 'success') {
-                    const imageUrl = photos.info.url;
-                    const url = process.env.REACT_APP_API_URL + `users/${userInfo.id}/update`;
-                    // gui du lieu len server
-                    axios.post(url, { [type]: imageUrl }).then(res => {
-                        console.log(res, 'oke');
-                        const newUserInfo = {
-                            ...userInfo,
-                            [type]: imageUrl
-                        }
-                        // chuyen userinfo cho component cha quan ly
-                        onUploadImage(newUserInfo);
-                    });
-                }
-            } else {
-                console.log(error);
-            }
-        })
-    }
+  function onHandleInputChange(event) {
+    const value = event.target.value;
+    setInputContent(value);
+  }
 
+  function onSave() {
+    const url = process.env.REACT_APP_API_URL + `users/${userInfo.id}/update`;
+    let newName = inputContent;
+    axios.post(url, { name: newName }).then((res) => {
+      window.location.reload(false);
+    });
+  }
 
-    return (
-        <div className="box-info" style={{ backgroundImage: `url(${userInfo.backgroundUrl})` }}>
-            {isMe && <div className="upload" onClick={() => beginUpload('backgroundUrl')}>
-                <img src={cameraSVG} alt="camera" />
-                <p>Cập nhật ảnh bìa</p>
-            </div>}
-            <div className="box-info__content">
-                {isMe && <div onClick={() => beginUpload('avatarUrl')} className="avatar" style={{ backgroundImage: `url(${userInfo.avatarUrl})` }}></div>}
-                {isMe || <div className="avatar" style={{ backgroundImage: `url(${userInfo.avatarUrl})` }}></div>}
-                <p className="name">{userInfo.name}</p>
-            </div>
-            {isMe && <div className="box-info__edit">
-                <img src={editSVG} alt="edit" />
-                <p>Cập nhật thông tin</p>
-            </div>}
+  return (
+    <div
+      className="box-info"
+      style={{ backgroundImage: `url(${userInfo.backgroundUrl})` }}
+    >
+      {isMe && (
+        <div className="upload" onClick={() => beginUpload("backgroundUrl")}>
+          <img src={cameraSVG} alt="camera" />
+          <p>Cập nhật ảnh bìa</p>
         </div>
-    );
+      )}
+      <div className="box-info__content">
+        {isMe && (
+          <div
+            onClick={() => beginUpload("avatarUrl")}
+            className="avatar"
+            style={{ backgroundImage: `url(${userInfo.avatarUrl})` }}
+          ></div>
+        )}
+        {isMe || (
+          <div
+            className="avatar"
+            style={{ backgroundImage: `url(${userInfo.avatarUrl})` }}
+          ></div>
+        )}
+        <p className="name">{userInfo.name}</p>
+      </div>
+      {isMe && (
+        <div
+          onClick={() => setStatusEditBox(!statusEditBox)}
+          className="box-info__edit"
+        >
+          <img src={editSVG} alt="edit" />
+          <p>Cập nhật thông tin</p>
+        </div>
+      )}
+      {statusEditBox && (
+        <div className="edit-box">
+          <input
+            onChange={onHandleInputChange}
+            value={inputContent}
+            type="text"
+            placeholder="Nhập tên mới ..."
+          />
+          <div className="button-box">
+            <button onClick={onSave} className="box-btn">
+              Lưu
+            </button>
+            <button onClick={() => setStatusEditBox(false)} className="box-btn">
+              Hủy
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default BoxInfo;
--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,82 +1,88 @@
-import React from 'react';
-import './style.scss';
-import Header from '../commons/Header/index';
-import NewFeed from '../commons/Newfeed/index';
-import BoxInfo from './BoxInfo/index';
-import Gallery from './Gallery/index';
-import { useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { useEffect } from 'react';
-import axios from 'axios';
-
-
-
+import React from "react";
+import "./style.scss";
+import Header from "../commons/Header/index";
+import NewFeed from "../commons/Newfeed/index";
+import BoxInfo from "./BoxInfo/index";
+import Gallery from "./Gallery/index";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import axios from "axios";
 
 function Profile(props) {
+  // default khi user chua co avartar hay backgroud image
+  const defaultBackgroundUrl =
+    "https://img2.pngio.com/default-png-5-png-image-default-png-1200_800.png";
+  const defaultAvatarUrl =
+    "https://img2.pngio.com/index-of-wp-content-themes-fgm21-child-assets-images-default-png-495_495.png";
 
-    // default khi user chua co avartar hay backgroud image
-    const defaultBackgroundUrl = 'https://img2.pngio.com/default-png-5-png-image-default-png-1200_800.png';
-    const defaultAvatarUrl = 'https://img2.pngio.com/index-of-wp-content-themes-fgm21-child-assets-images-default-png-495_495.png';
+  const [userInfo, setUserInfo] = useState({});
+  const { userId } = useParams();
+  let getUserInfo = {}; // info cua nick dang dang nhap
+  if (localStorage.getItem("userInfo")) {
+    getUserInfo = JSON.parse(localStorage.getItem("userInfo"));
+  }
+  // setstorage
 
-    const [userInfo, setUserInfo] = useState({});
-    const { userId } = useParams();
-    let getUserInfo = {}; // info cua nick dang dang nhap
-    if (localStorage.getItem('userInfo')) {
-        getUserInfo = JSON.parse(localStorage.getItem('userInfo'));
-    }
-    // setstorage
+  useEffect(() => {
+    const url = process.env.REACT_APP_API_URL + `users/${userId}/info`;
+    axios.get(url).then((res) => {
+      let newUserInfo = {
+        ...res.data,
+      };
+      delete newUserInfo._id;
+      newUserInfo = {
+        ...newUserInfo,
+        id: userId,
+      };
+      // lay anh default neu chua co anh
+      if (!newUserInfo.backgroundUrl) {
+        newUserInfo = {
+          ...newUserInfo,
+          backgroundUrl: defaultBackgroundUrl,
+        };
+      }
+      if (!newUserInfo.avatarUrl) {
+        newUserInfo = {
+          ...newUserInfo,
+          avatarUrl: defaultAvatarUrl,
+        };
+      }
+      setUserInfo(newUserInfo);
+    });
+  }, [userId]);
 
-    useEffect(() => {
-        const url = process.env.REACT_APP_API_URL + `users/${userId}/info`;
-        axios.get(url).then(res => {
-            let newUserInfo = {
-                ...res.data
-            };
-            delete newUserInfo._id;
-            newUserInfo = {
-                ...newUserInfo,
-                id: userId
-            };
-            // lay anh default neu chua co anh
-            if (!newUserInfo.backgroundUrl) {
-                newUserInfo = {
-                    ...newUserInfo,
-                    backgroundUrl: defaultBackgroundUrl,
-                };
-            }
-            if (!newUserInfo.avatarUrl) {
-                newUserInfo = {
-                    ...newUserInfo,
-                    avatarUrl: defaultAvatarUrl,
-                };
-            }
-            setUserInfo(newUserInfo);
-        })
-    }, [userId]);
+  function onUploadImage(newUserInfo) {
+    localStorage.setItem("userInfo", JSON.stringify(newUserInfo));
+    setUserInfo(newUserInfo);
+  }
 
-    function onUploadImage(newUserInfo) {
-        localStorage.setItem('userInfo', JSON.stringify(newUserInfo));
-        setUserInfo(newUserInfo);
-    }
-
-    return (
-        <div className="profile-wrap">
-            <Header userLogined={getUserInfo} />
-            <div className="profile__main">
-                <div className="container">
-                    <BoxInfo userInfo={userInfo} onUploadImage={onUploadImage} isMe={getUserInfo.id === userId ? true : false} />
-                    <div className="content">
-                        <div className="newfeed-wrap">
-                            <NewFeed userInfo={userInfo} type="profile" currentUserInfo={getUserInfo} />
-                        </div>
-                        <div className="gallery-wrap">
-                            <Gallery />
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="profile-wrap">
+      <Header userLogined={getUserInfo} />
+      <div className="profile__main">
+        <div className="container">
+          <BoxInfo
+            userInfo={userInfo}
+            onUploadImage={onUploadImage}
+            isMe={getUserInfo.id === userId ? true : false}
+          />
+          <div className="content">
+            <div className="newfeed-wrap">
+              <NewFeed
+                userInfo={userInfo}
+                type="profile"
+                currentUserInfo={getUserInfo}
+              />
             </div>
+            <div className="gallery-wrap">
+              <Gallery userInfo={userInfo} />
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Profile;
