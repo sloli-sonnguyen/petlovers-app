5bed2356b5248706cefccdab6ec691561219d70f
sloli-sonnguyen <sloli.sonnguyen@gmail.com>
2020-07-27 11:28:07 +0700

    Updates

---
+++ precache-manifest.4d020c7cf354979fc79584bd0b32a61c.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e502efe4d059308e0ff4a147d747a76c",
+    "url": "/%7Bpetlovers-app-frontend%7D/index.html"
+  },
+  {
+    "revision": "0712a024b9ee8bd083a2",
+    "url": "/%7Bpetlovers-app-frontend%7D/static/css/main.a759fdd7.chunk.css"
+  },
+  {
+    "revision": "522bb75aa6450f67356b",
+    "url": "/%7Bpetlovers-app-frontend%7D/static/js/2.dd58b4b5.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/%7Bpetlovers-app-frontend%7D/static/js/2.dd58b4b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0712a024b9ee8bd083a2",
+    "url": "/%7Bpetlovers-app-frontend%7D/static/js/main.078f14a1.chunk.js"
+  },
+  {
+    "revision": "5d6e06db5133514b0df9",
+    "url": "/%7Bpetlovers-app-frontend%7D/static/js/runtime-main.bad51216.js"
+  },
+  {
+    "revision": "0b3a9c063f7a5069cb30dd9b6c6e4f36",
+    "url": "/%7Bpetlovers-app-frontend%7D/static/media/ball.0b3a9c06.svg"
+  },
+  {
+    "revision": "ffb4c987bcfda41b7516968496c72c4c",
+    "url": "/%7Bpetlovers-app-frontend%7D/static/media/bell.ffb4c987.svg"
+  },
+  {
+    "revision": "1e54352ac9157e5344dbb6bb26c6b0f2",
+    "url": "/%7Bpetlovers-app-frontend%7D/static/media/bone.1e54352a.svg"
+  },
+  {
+    "revision": "a5377696644f054a6a4279b8a2ea5bbc",
+    "url": "/%7Bpetlovers-app-frontend%7D/static/media/camera.a5377696.svg"
+  },
+  {
+    "revision": "ee5d623838464c68ea8b4de8cc9ce422",
+    "url": "/%7Bpetlovers-app-frontend%7D/static/media/cat.ee5d6238.svg"
+  },
+  {
+    "revision": "9f288b75f29cef45720f570e2c62bfa0",
+    "url": "/%7Bpetlovers-app-frontend%7D/static/media/catAvatar.9f288b75.jpg"
+  },
+  {
+    "revision": "256b6e53b2d5f05350f7db4126e20f37",
+    "url": "/%7Bpetlovers-app-frontend%7D/static/media/catsleep.256b6e53.png"
+  },
+  {
+    "revision": "55da0b1fc83603b4be9c303646f380d7",
+    "url": "/%7Bpetlovers-app-frontend%7D/static/media/comment.55da0b1f.svg"
+  },
+  {
+    "revision": "0679ff7ca65afc162633607a7f23de75",
+    "url": "/%7Bpetlovers-app-frontend%7D/static/media/edit.0679ff7c.svg"
+  },
+  {
+    "revision": "88b23f77c7c299c68c2055f84bbfdd06",
+    "url": "/%7Bpetlovers-app-frontend%7D/static/media/food.88b23f77.svg"
+  },
+  {
+    "revision": "6ade051af40269cb1ea77e6fc47f2b58",
+    "url": "/%7Bpetlovers-app-frontend%7D/static/media/gallery.6ade051a.svg"
+  },
+  {
+    "revision": "a8cd76c1934dc020e510f93a33774305",
+    "url": "/%7Bpetlovers-app-frontend%7D/static/media/heart-active.a8cd76c1.svg"
+  },
+  {
+    "revision": "70ebbcd163470cdcdd46617ad9e91610",
+    "url": "/%7Bpetlovers-app-frontend%7D/static/media/heart.70ebbcd1.svg"
+  },
+  {
+    "revision": "6606c14dd454bc90f2dfd992733ec258",
+    "url": "/%7Bpetlovers-app-frontend%7D/static/media/home.6606c14d.svg"
+  },
+  {
+    "revision": "e03fdaf3377fec5f06c1f352d6fb269e",
+    "url": "/%7Bpetlovers-app-frontend%7D/static/media/logout.e03fdaf3.svg"
+  },
+  {
+    "revision": "123274443fc523569559a4bdb1c75fad",
+    "url": "/%7Bpetlovers-app-frontend%7D/static/media/open-menu.12327444.svg"
+  },
+  {
+    "revision": "3a1ffe6b4b37520be54ecec957266948",
+    "url": "/%7Bpetlovers-app-frontend%7D/static/media/save.3a1ffe6b.svg"
+  },
+  {
+    "revision": "a0c5eda5b1b05dd5fe97f44a5c0894a4",
+    "url": "/%7Bpetlovers-app-frontend%7D/static/media/search.a0c5eda5.svg"
+  },
+  {
+    "revision": "accbd70d2311a3ea2fe1284e7d271c89",
+    "url": "/%7Bpetlovers-app-frontend%7D/static/media/user.accbd70d.svg"
+  },
+  {
+    "revision": "54e8062ae780ef9d3cf4ff908d9a3561",
+    "url": "/%7Bpetlovers-app-frontend%7D/static/media/world.54e8062a.svg"
+  }
+]);
